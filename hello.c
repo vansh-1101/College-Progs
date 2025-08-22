cfd1932ba70d5ffe8bfde0e566cce2e3543a06de
Vansh Darji <darjivansh1101@gmail.com>
2025-08-22 18:17:59 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main() {
     printf("Hello, World!\n");
+
+    printf("Enter First Value:=");
+    scanf("%d",&a);
+    
+    printf("Enter Second Value:=");
+    scanf("%d",&b);
+    
     return 0;
 }
