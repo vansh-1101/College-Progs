a822e7680281915c25f236a0f9b200f7f19a7bcb
Vansh Darji <darjivansh1101@gmail.com>
2025-08-22 18:15:08 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    // Added printGreeting method
+    public static void printGreeting() {
+        System.out.println("Greetings from Java!");
+    }
+}
